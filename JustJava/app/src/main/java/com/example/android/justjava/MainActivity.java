60e71d666ac260c7f51b264e00fca6d680a06ddc
davidmigloz <me@davidmiguel.com>
2016-09-03 11:43:25 +0200

    Minor changes in JustJava app

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.android.justjava;
 import java.text.NumberFormat;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private int quantity = 2;
 
@@ -22,8 +22,7 @@ public class MainActivity extends Activity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String msg = "Total: " + NumberFormat.getCurrencyInstance().format(quantity * 5) + "\nThank you!";
-        displayMessage(msg);
+        displayMessage(calculateOrderSummary(quantity, calculatePrice(quantity)));
     }
 
     /**
@@ -35,28 +34,39 @@ public class MainActivity extends Activity {
     }
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
-    public void increment(View view){
+    public void increment(View view) {
         quantity++;
         display(quantity);
     }
 
-    public void decrement(View view){
+    public void decrement(View view) {
         quantity--;
         display(quantity);
+    }
+
+    /**
+     * Calculates the price of the order based on the current quantity.
+     *
+     * @return the price
+     */
+    private int calculatePrice(int quantity) {
+        int price = quantity * 5;
+        return price;
+    }
+
+    private String calculateOrderSummary(int quantity, int price) {
+        String msg;
+        msg = "Name: Kaptain Kunal\n";
+        msg += "Quantity: " + quantity + "\n";
+        msg += "Total: " + NumberFormat.getCurrencyInstance().format(price) + "\n";
+        msg += "Thank you!";
+        return msg;
     }
 }
